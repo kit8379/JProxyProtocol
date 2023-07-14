1758e046252c1c64c59b2010ce5ede67acd058ae
kit8379 <manhhkit@gmail.com>
2023-07-14 23:27:24 +0800

    test

--- src/JProxyProtocol.java
+++ src/JProxyProtocol.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,25 +20,57 @@ public class JProxyProtocol {
         int listenPort = Integer.parseInt(prop.getProperty("listen.port"));
         String targetIp = prop.getProperty("target.ip");
         int targetPort = Integer.parseInt(prop.getProperty("target.port"));
+        boolean isChainedProxy = Boolean.parseBoolean(prop.getProperty("proxy.chained"));
 
         logger.log(Level.INFO, "Starting JProxyProtocol...");
+        System.out.println("JProxyProtocol has started. Done!");
+
         try (ServerSocket serverSocket = new ServerSocket(listenPort)) {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
+                String clientIp = clientSocket.getInetAddress().getHostAddress();
 
-                Socket targetSocket = new Socket(targetIp, targetPort);
-                OutputStream targetOut = targetSocket.getOutputStream();
+                // Log connection made
+                logger.log(Level.INFO, "Connection made from: " + clientIp);
 
-                // Write Proxy Protocol v1 header
-                String header = "PROXY TCP4 " + clientSocket.getInetAddress().getHostAddress() + " "
-                        + targetSocket.getInetAddress().getHostAddress() + " " + clientSocket.getPort() + " "
-                        + targetSocket.getPort() + "\r\n";
-                targetOut.write(header.getBytes());
-                targetOut.flush();
+                // Handle each connection in a new thread
+                new Thread(() -> {
+                    try (Socket targetSocket = new Socket(targetIp, targetPort)) {
+                        OutputStream targetOut = targetSocket.getOutputStream();
 
-                // Forward data between client and server
-                new Thread(new Forwarder(clientSocket.getInputStream(), targetOut)).start();
-                new Thread(new Forwarder(targetSocket.getInputStream(), clientSocket.getOutputStream())).start();
+                        // Write Proxy Protocol v1 header
+                        String header;
+                        String finalClientIp = clientIp;
+                        if (isChainedProxy) {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            String proxyHeader = reader.readLine(); // Proxy Protocol header from client
+                            String[] parts = proxyHeader.split(" ");
+                            if (parts.length < 6) {
+                                throw new IOException("Invalid Proxy Protocol header from client");
+                            }
+                            finalClientIp = parts[2];
+                        }
+                        header = "PROXY TCP4 " + finalClientIp + " " + targetSocket.getInetAddress().getHostAddress() + " "
+                                + clientSocket.getPort() + " " + targetSocket.getPort() + "\r\n";
+                        targetOut.write(header.getBytes());
+                        targetOut.flush();
+
+                        // Forward data between client and server
+                        Thread t1 = new Thread(new Forwarder(clientSocket.getInputStream(), targetOut, clientSocket, targetSocket));
+                        Thread t2 = new Thread(new Forwarder(targetSocket.getInputStream(), clientSocket.getOutputStream(), targetSocket, clientSocket));
+
+                        t1.start();
+                        t2.start();
+
+                        t1.join();
+                        t2.join();
+
+                    } catch (Exception e) {
+                        logger.log(Level.SEVERE, "An exception occurred in the main method.", e);
+                    } finally {
+                        logger.log(Level.INFO, "Connection disconnected from: " + clientIp); // Log connection disconnected
+                    }
+                }).start();
             }
         } catch (Exception e) {
             logger.log(Level.SEVERE, "An exception occurred in the main method.", e);
@@ -48,26 +81,36 @@ public class JProxyProtocol {
 class Forwarder implements Runnable {
     private final InputStream in;
     private final OutputStream out;
+    private final Socket inSocket;
+    private final Socket outSocket;
 
-    Forwarder(InputStream in, OutputStream out) {
+    Forwarder(InputStream in, OutputStream out, Socket inSocket, Socket outSocket) {
         this.in = in;
         this.out = out;
+        this.inSocket = inSocket;
+        this.outSocket = outSocket;
     }
 
     @Override
     public void run() {
+        byte[] buffer = new byte[4096];
         try {
-            byte[] buffer = new byte[4096];
             int read;
             while ((read = in.read(buffer)) != -1) {
                 out.write(buffer, 0, read);
             }
+        } catch (SocketException se) {
+            // Socket has been closed, handle this scenario here
         } catch (Exception e) {
             JProxyProtocol.logger.log(Level.SEVERE, "An exception occurred while forwarding data.", e);
         } finally {
             try {
-                in.close();
-                out.close();
+                if(!inSocket.isOutputShutdown()) {
+                    inSocket.shutdownOutput();
+                }
+                if(!outSocket.isOutputShutdown()) {
+                    outSocket.shutdownOutput();
+                }
             } catch (Exception e) {
                 JProxyProtocol.logger.log(Level.SEVERE, "An exception occurred while closing streams.", e);
             }
